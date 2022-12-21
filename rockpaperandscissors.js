27190536d206fd39036c15f1d02b4d89ef75b294
Jonas Lima <jonaslimagt@gmail.com>
2022-12-21 12:00:49 -0300

    Fix computerChoice bug and Tie bug

--- rockpaperandscissors.js
+++ rockpaperandscissors.js
@@ -1,31 +1,34 @@
 const game_options = [`rock`, `paper`, `scissors`]; //for computer choice in getComputerChoice() function
 
 function playRound(playerSelection, computerSelection){//Make a round of Rock Paper Scissors
+    console.log(`You chose ` + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1));
+    console.log(`Computer chose ` + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1));
     if (playerSelection === computerSelection){
         console.log(`Tie! Seems we have a ` + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + ` situation!`);
+        return 2;
     } else if (playerSelection === `rock`){
         if (computerSelection === `paper`){
             console.log(`You Lose! Paper beats Rock`);
-            return false;
+            return 0;
         } else {
             console.log(`You Win! Rock beats Scissors`);
-            return true;
+            return 1;
         }
     } else if (playerSelection === `paper`){
         if (computerSelection === `scissors`){
             console.log(`You Lose! Scissors beats Paper`);
-            return false;
+            return 0;
         } else {
             console.log(`You Win! Paper beats Rock`);
-            return true;
+            return 1;
         }
     } else {
         if (computerSelection === `rock`){
             console.log(`You Lose! Rock beats Scissors`);
-            return false;
+            return 0;
         } else {
             console.log(`You Win! Scissors beats Paper`);
-            return true;
+            return 1;
         }
     }
 }
@@ -42,13 +45,21 @@ let computerScore = 0;
 for (let i = 1; i <= 5; i++){
     console.log(`Round ` + i);
     playerChoice = prompt(`Rock, Paper or Scissors? `);
-    let computerAux = Math.floor(Math.random() * 3) + 1;
+    let computerAux = Math.floor(Math.random() * 3);
+    console.log(computerAux);
     computerChoice = game_options[computerAux];
     let matchScore = playRound(playerChoice, computerChoice);
-    if (matchScore === true){
-        playerScore++;
-    } else {
-        computerScore++;
+    switch(matchScore){
+        case 0:
+            computerScore++;
+            break;
+        case 1:
+            playerScore++;
+            break;
+        case 2:
+            playerScore++;
+            computerScore++;
+            break;
     }
-    console.log(`Player ` + playerScore + ` ✕ ` + computerScore ` Computer`);
+    console.log(`Player ` + playerScore + ` x ` + computerScore + ` Computer`);
 }
